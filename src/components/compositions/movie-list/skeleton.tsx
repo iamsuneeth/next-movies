8dc43656e9210ecb45691ffdc06b7670376d637b
iamsuneeth <suneeth.lenin@gmail.com>
2024-03-24 21:47:47 +0530

    fix: skeleton

--- src/components/compositions/movie-list/skeleton.tsx
+++ src/components/compositions/movie-list/skeleton.tsx
@@ -8,6 +8,7 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { GridItem } from '@styled-system/jsx';
 import { token } from '@styled-system/tokens';
+import { css } from '@styled-system/css';
 
 const skeletonArray = Array(8).fill(0);
 
@@ -17,15 +18,22 @@ export const MovieListSkeleton = () => {
       {skeletonArray.map((index) => (
         <GridItem key={index}>
           <BorderlessCard>
-            <CardHeader>
+            <CardHeader
+              className={css({
+                paddingInline: 0,
+              })}
+            >
               <Skeleton
-                width={272}
-                height={408}
+                height={'calc(var(--grid-item-min-width) * 1.8)'}
                 baseColor={token('colors.skeleton.base')}
                 highlightColor={token('colors.skeleton.highlight')}
               />
             </CardHeader>
-            <CardContent>
+            <CardContent
+              className={css({
+                paddingInline: 0,
+              })}
+            >
               <Skeleton
                 count={1.5}
                 baseColor={token('colors.skeleton.base')}
