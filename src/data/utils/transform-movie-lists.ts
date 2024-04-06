aaed6f03a40f619c37b27fd9b2730b3a53bed6e9
iamsuneeth <suneeth.lenin@gmail.com>
2024-04-06 13:38:44 +0530

    temp

--- src/data/utils/transform-movie-lists.ts
+++ src/data/utils/transform-movie-lists.ts
@@ -17,3 +17,117 @@ export function transformMovieList(movieList: MovieListResponse) {
     })),
   };
 }
+
+//
+const layoutdata = {
+  componentCategory: 'layout',
+  componentId: 'Fragment',
+  children: [
+    {
+      componentCategory: 'container',
+      componentId: 'MovieListGridContainer',
+      children: [
+        {
+          componentCategory: 'link',
+          componentId: 'Link',
+          props: {
+            href: '/movie/${data.results.[].id}',
+          },
+          children: [
+            {
+              componentCategory: 'container',
+              componentId: 'BorderlessCard',
+              children: [
+                {
+                  componentCategory: 'container',
+                  componentId: 'CardHeader',
+                  children: [
+                    {
+                      componentCategory: 'container',
+                      componentId: 'AspectRatio',
+                      props: {
+                        width: '342',
+                        height: '513',
+                      },
+                      children: [
+                        {
+                          componentCategory: 'media',
+                          componentId: 'Image',
+                          props: {
+                            src: '${data.results.[].poster_path}',
+                            alt: '${data.results.[].title}',
+                            width: '342',
+                            height: '513',
+                          },
+                        },
+                      ],
+                    },
+                    {
+                      componentCategory: 'container',
+                      componentId: 'CardContent',
+                      children: [
+                        {
+                          componentCategory: 'text',
+                          componentId: 'CardTitle',
+                          props: {
+                            children: '${data.results.[].title}',
+                          },
+                        },
+                        {
+                          componentCategory: 'element',
+                          componentId: 'Rating',
+                          props: {
+                            value: '${data.results.[].vote_average}',
+                          },
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      componentCategory: 'element',
+      componentId: 'MoviePagination',
+      props: {
+        maxPage: '${data.total_pages}',
+        page: '${paginationData.page}',
+      },
+    },
+  ],
+};
+
+
+class LayoutConfigVisitor {
+  data;
+  constructor(layoutData:any, paginationData: any) {
+    this.data = {
+      layoutData,
+      paginationData
+    }
+  }
+  populateProps(props:Record<string, string|number|boolean>, index?: number) {
+    Object.entries(props).forEach(([key, value]) => {
+      if(typeof value !== 'string') return;
+      props[key] = value.replace(/\${(.*?)}/g, (_, firstMatch) => {
+        const tokens = firstMatch.split('.');
+
+      })
+    })
+  }
+  visit(layoutTree) {
+
+    const props = 
+
+    return {
+      ...layoutTree,
+      props
+      
+    }
+
+  }
+}
